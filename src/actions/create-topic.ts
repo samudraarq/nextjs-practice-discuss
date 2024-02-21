fffcd27d2afcf5275a37faa722a56579466a19ba
Samudra Faris Arqam <samudrafaris@gmail.com>
2024-02-21 14:42:38 +0700

    add create topic modal

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,10 @@
 "use server";
 
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
+  const name = formData.get("name");
+  const description = formData.get("description");
+
+  console.log(`Creating topic: ${name} - ${description}`);
+
   // TODO: revalidate the homepage
 }
