5a8bf62ed40fd50b9f86dfb1bd895454f22a047a
Samudra Faris Arqam <samudrafaris@gmail.com>
2024-02-21 15:35:28 +0700

    install zod & create topic actions

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,79 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
 
-  console.log(`Creating topic: ${name} - ${description}`);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
 
-  // TODO: revalidate the homepage
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to create a topic"],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath(paths.home());
+  redirect(paths.topicShow(topic.slug));
 }
